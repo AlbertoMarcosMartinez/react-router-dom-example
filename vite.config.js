7b7e3d6c21389dfbd02155258f5b0c82274a14e4
Alberto A. Marcos Martinez <AlbertoMarcosMartinez@outlook.com>
2025-05-15 13:17:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/react-router-dom-example',
+  }
+});
